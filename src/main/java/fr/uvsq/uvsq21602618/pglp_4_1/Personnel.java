f830c0b39c67889628f2eab1111c951f655debdb
Nathalie BERNARD <nathalie.bernard@ens.uvsq.fr>
2020-03-04 11:37:24 +0100

    finalisation des methodes de builder

--- src/main/java/fr/uvsq/uvsq21602618/pglp_4_1/Personnel.java
+++ src/main/java/fr/uvsq/uvsq21602618/pglp_4_1/Personnel.java
@@ -8,14 +8,26 @@ public class Personnel {
 	private final String prenom;
 	private final String fonction;
 	private final LocalDate dateNaissance;
-	private final ArrayList<NumeroTelephone> telephone;
+	private final ArrayList<NumeroTelephone> numTelephones;
 	
 	public static class Builder {
 		private final String nom;
 		private final String prenom;
 		private final String fonction;
 		private final LocalDate dateNaissance;
-		private final ArrayList<NumeroTelephone> telephone;
+		private final ArrayList<NumeroTelephone> numTelephones;
+		
+		public Builder(String nom, String prenom, String fonction, LocalDate dateNaissance, ArrayList<NumeroTelephone> numTelephones) {
+			this.nom = nom;
+			this.prenom = prenom;
+			this.fonction = fonction;
+			this.dateNaissance= dateNaissance;
+			this.numTelephones= numTelephones;
+		}
+		
+		public Personnel build() {
+			return new Personnel(this);
+		}
 	}
 	
 	private Personnel(Builder builder) {
