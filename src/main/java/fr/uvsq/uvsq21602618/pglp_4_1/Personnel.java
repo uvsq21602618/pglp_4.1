1c0b3efd06330f8ad583a6456939bf4aefd3e731
Nathalie BERNARD <nathalie.bernard@ens.uvsq.fr>
2020-03-04 11:11:47 +0100

    creation classe imbriquee Builder

--- src/main/java/fr/uvsq/uvsq21602618/pglp_4_1/Personnel.java
+++ src/main/java/fr/uvsq/uvsq21602618/pglp_4_1/Personnel.java
@@ -1,5 +1,26 @@
 package fr.uvsq.uvsq21602618.pglp_4_1;
 
-public class Personnel {
+import java.time.LocalDate;
+import java.util.ArrayList;
 
+public class Personnel {
+	private final String nom;
+	private final String prenom;
+	private final String fonction;
+	private final LocalDate dateNaissance;
+	private final ArrayList<NumeroTelephone> telephone;
+	
+	public static class Builder {
+		private final String nom;
+		private final String prenom;
+		private final String fonction;
+		private final LocalDate dateNaissance;
+		private final ArrayList<NumeroTelephone> telephone;
+	}
+	
+	private Personnel(Builder builder) {
+		nom = builder.nom;
+		prenom = builder.prenom;
+		
+	}
 }
