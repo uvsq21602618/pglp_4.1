e69588a47fc6f1872bcaef6bdf94bc81bf959a70
Nathalie BERNARD <nathalie.bernard@ens.uvsq.fr>
2020-03-04 11:49:38 +0100

    creation du singleton AppSingleton

---
+++ src/main/java/fr/uvsq/uvsq21602618/pglp_4_1/AppSingleton.java
@@ -0,0 +1,13 @@
+package fr.uvsq.uvsq21602618.pglp_4_1;
+
+public enum AppSingleton {
+	ENVIRONNEMENT;
+	
+	public void run(String[] args) {
+		//
+	}
+	
+	public static void main(String[] args) {
+		ENVIRONNEMENT.run(args);
+	}
+}
