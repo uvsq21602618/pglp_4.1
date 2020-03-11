ed99bc332dfd0554b922cd385dcfabdc95908366
nathalie bernard <nathalie.bernard78@outlook.fr>
2020-03-11 10:01:48 +0100

    creation interface Composant

--- src/main/java/fr/uvsq/uvsq21602618/pglp_4_1/AppSingleton.java
+++ src/main/java/fr/uvsq/uvsq21602618/pglp_4_1/AppSingleton.java
@@ -1,10 +1,18 @@
 package fr.uvsq.uvsq21602618.pglp_4_1;
 
+import java.time.LocalDate;
+
+import fr.uvsq.uvsq21602618.pglp_4_1.Personnel.Builder;
+
 public enum AppSingleton {
 	ENVIRONNEMENT;
 	
 	public void run(String[] args) {
-		//
+		NumeroTelephone portable = new NumeroTelephone("portable","0651624519");
+		Builder p = new Personnel.Builder("SMITH","John", "secrétaire", LocalDate.of(1964,8,25));
+		p.numTelephones(portable);
+		p.build();
+		
 	}
 	
 	public static void main(String[] args) {
