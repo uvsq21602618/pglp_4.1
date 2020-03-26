d59882fc4740260824a08f7a7001f9bb16ba5ff1
nathalie bernard <nathalie.bernard@ens.uvsq.fr>
2020-03-26 18:28:17 +0100

    ajout de package-info dans test

--- src/test/java/fr/uvsq/uvsq21602618/pglp_4_1/PersonnelTest.java
+++ src/test/java/fr/uvsq/uvsq21602618/pglp_4_1/PersonnelTest.java
@@ -14,31 +14,32 @@ import fr.uvsq.uvsq21602618.pglp_4_1.Personnel.Builder;
  */
 public class PersonnelTest {
     /**
-     * Teste le constructeur de Personnel utilisant 
+     * Teste le constructeur de Personnel utilisant
      * le pattern Builder.
      */
     @Test
-    public void ConstructeurTest() {
+    public void constructeurTest() {
         NumeroTelephone portable =
                 new NumeroTelephone("portable", "0651624519");
         Builder b = new Builder("SMITH", "John", "secrétaire",
                 LocalDate.of(1964, 8, 25));
         b.numTelephones(portable);
         Personnel secretaire = b.build();
-        
+
         String expDesc = "portable";
         String expNum = "0651624519";
         String expNom = "SMITH";
         String expPrenom = "John";
         String expFonction = "secrétaire";
         LocalDate expDate = LocalDate.of(1964, 8, 25);
-        
-        assertEquals(expDesc, secretaire.getNumTelephones().get(0).getDescriptif());
+
+        assertEquals(expDesc,
+                secretaire.getNumTelephones().get(0).getDescriptif());
         assertEquals(expNum, secretaire.getNumTelephones().get(0).getNumero());
         assertEquals(expNom, secretaire.getNom());
         assertEquals(expPrenom, secretaire.getPrenom());
         assertEquals(expFonction, secretaire.getFonction());
         assertEquals(expDate, secretaire.getDateNaissance());
-        
+
     }
 }
